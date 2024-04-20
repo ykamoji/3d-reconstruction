657290ef0bc764839ee0199c54876db8287cd3c8
ykamoji <ykamoji@umass.edu>
2024-04-19 20:04:50 -0400

    Modified training to use trained models

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import yaml
 import json
-import torch
 from lightning import Trainer
 from lightning.pytorch.callbacks import ModelCheckpoint
 from renderer.utils import CustomObject, get_yaml_loader
@@ -12,8 +11,6 @@ from generator_model import initialize_model
 
 def train(config):
 
-    rendering_kwargs = config.rendering.triplane_renderer_config.rendering_kwargs
-    mlp_decoder_config = config.rendering.triplane_renderer_config.mlp_decoder_config
     results_folder = config.logging.save_dir
     render_3d = config.rendering.render
     unet_feature_dim = config.unet_feature_dim
@@ -27,7 +24,8 @@ def train(config):
         else:
             renderer = ImportanceRenderer()
 
-        eg3d_decoder = OSGDecoder_extended(unet_feature_dim, options=mlp_decoder_config)
+        eg3d_decoder = OSGDecoder_extended(unet_feature_dim,
+                                           options=config.rendering.triplane_renderer_config.mlp_decoder_config)
         unet_out_dim = config.unet_feature_dim * 3
     else:
         renderer = None
@@ -40,7 +38,7 @@ def train(config):
         out_dim=unet_out_dim,
         renderer=renderer,
         eg3d_decoder=eg3d_decoder,
-        rendering_options=rendering_kwargs,
+        rendering_options=config.rendering.triplane_renderer_config.rendering_kwargs,
         dim_mults=(1, 2, 4, 8),
         render_3d=render_3d,
         image_size=config.image_size,
@@ -51,7 +49,6 @@ def train(config):
         dirpath=save_dir_checkpoints,
         save_on_train_epoch_end=True,
         enable_version_counter=True,
-        # save_weights_only=True,
         save_top_k=1,
         verbose=True,
         monitor='total_loss_step',
@@ -61,7 +58,6 @@ def train(config):
     )
 
     trainer = Trainer(max_epochs=config.training.train_num_steps,
-                      max_steps=1,
                       log_every_n_steps=1,
                       # precision="16-mixed",
                       default_root_dir=results_folder,
@@ -69,16 +65,14 @@ def train(config):
 
     ## TODO:: Freeze the encode and parts of decoder of the unet model
     generator3DModel = initialize_model(config, model_unet)
-    model = generator3DModel()
 
-    ## TODO:: Fix loading module
     if load_model:
-        pass
-        # load_model = f'{config.logging.model_path}/checkpoint_generic.pt'
-        # checkpoint = torch.load(load_model, map_location='mps')
+        # checkpoint = torch.load(f'{config.logging.model_path}', map_location=config.device)
         # state_dict = {k.partition('module.')[2]: checkpoint['model'][k] for k in checkpoint['model'].keys()}
         # model_unet.load_state_dict(state_dict)
-        # model.load_from_checkpoint(f'{config.logging.model_path}',map_location=config.device)
+        model = generator3DModel.load_from_checkpoint(f'{config.logging.model_path}', map_location=config.device)
+    else:
+        model = generator3DModel()
 
     trainer.fit(model)
 
